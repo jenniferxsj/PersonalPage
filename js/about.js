f953a0e786a72db7e7ca9172bd7aa10ab0eaa7a3
jenniferxsj <xiao.57578@gmail.com>
2021-10-09 17:03:46 -0700

    inital commit

---
+++ js/about.js
@@ -0,0 +1,13 @@
+function newPage1() {
+  window.open("https://www.youtube.com/watch?v=jw-DWibbQO4&ab_channel=JenniferXiao");
+}
+
+function newPage2() {
+  window.open("https://github.com/jenniferxsj/CS5001_Project");
+}
+
+function snow() {
+  document.body.style.backgroundImage = "url('../img/bluesnow.png')";
+  document.getElementById('sidenav').style.backgroundColor = '#fffbf9';
+  document.getElementById('header-section').style.backgroundColor = '#e5f2ff';
+}
