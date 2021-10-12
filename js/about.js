08a7e71de69779b7bb2a88054f9effa5797922f1
Zhenghao Lin <gyouza_azuoyg@outlook.com>
2021-10-12 16:25:56 -0700

    Added some function comments

--- js/about.js
+++ js/about.js
@@ -1,11 +1,14 @@
+// Open the view demo
 function newPage1() {
   window.open("https://www.youtube.com/watch?v=jw-DWibbQO4&ab_channel=JenniferXiao");
 }
 
+// Open the Github homepage
 function newPage2() {
   window.open("https://github.com/jenniferxsj/CS5001_Project");
 }
 
+// Change the background into "snow" style
 function snow() {
   document.body.style.backgroundImage = "url('./img/bluesnow.png')";
   document.getElementById("sidenav").style.backgroundColor = "#fffbf9";
